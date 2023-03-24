0462915845698a547ebcad4efa9327bb35d5df80
Dilraj Somel <dsomel21@gmail.com>
2023-03-23 21:31:04 -0400

    Install React and related dependencies for the Slide Out

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), react()],
 });
