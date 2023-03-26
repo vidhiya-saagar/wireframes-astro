8559c2b958a63c08854b0dd5b4c6a6572a909db7
Dilraj Somel <dsomel21@gmail.com>
2023-03-26 19:52:28 -0400

    Clean up Types and stuff

--- src/store/chapterPreviewStore.ts
+++ src/store/chapterPreviewStore.ts
@@ -7,9 +7,8 @@ interface ChapterInfo {
   enTitle: string;
   enShortSummary: string;
   artworkUrl: string;
-  setOpenPreview: Function;
   bookName: string;
 }
 
-export const isOpen = atom(false);
-export const chapterToDisplay: null | ChapterInfo = atom(null);
+export const isOpen = atom<boolean>(false);
+export const chapterToDisplay = atom<ChapterInfo | null>(null);
