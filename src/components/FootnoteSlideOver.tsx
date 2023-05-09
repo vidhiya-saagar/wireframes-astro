151e13bbf72fd965e6a01e71268305a7af342769
Dilraj Somel <dsomel21@gmail.com>
2023-05-09 19:03:47 -0400

    Release chapter 2

--- src/components/FootnoteSlideOver.tsx
+++ src/components/FootnoteSlideOver.tsx
@@ -23,8 +23,16 @@ const contentfulOptions = {
   renderNode: {
     [BLOCKS.EMBEDDED_ASSET]: (node) => {
       const { title, description } = node.data.target.fields;
-      const { url } = node.data.target.fields.file;
-      return (
+      const { url, contentType } = node.data.target.fields.file;
+      const isPdf = contentType === 'application/pdf';
+
+      return isPdf ? (
+        <div>
+          <a href={url} download>
+            {title}
+          </a>
+        </div>
+      ) : (
         <figure>
           <img src={url} alt={title} loading="lazy" />
           {description && <figcaption>{description}</figcaption>}
