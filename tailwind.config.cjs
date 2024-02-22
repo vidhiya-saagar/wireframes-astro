3980c30ebbcecebc0ac9f5fdaebb4edd006b8b6b
Dilraj Somel <dsomel21@gmail.com>
2024-02-21 21:44:13 -0500

    Daisy config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,21 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['cyberpunk'], // GOOD
+    // themes: ['aqua'], // SUCKS
+    // themes: ['synthwave'], // OK
+    // themes: ['retro'], // GOOD
+    // themes: ['valentine'], // GOOD
+    // themes: ['halloween'], // GOOD
+    // themes: ['forest'], // GOOD
+    // themes: ['dracula'], // GOOD
+    // themes: ['luxury'], // sucky
+    // themes: ['coffee'], // GOOD
+    // themes: ['lemonade'], // GOOD
+    // themes: ['sunset'], // sucky
+    // themes: ['dim'], // sucky
+    // themes: ['autumn'], // sucky
+  },
 };
